051cafa14bbeaf8a583663703a08b1f5fff0aa1b
Ajeet <ajeetmdl23@gmail.com>
2023-02-17 12:46:59 -0500

    Update app.R

--- shinyR_HBCC_FigShare/app.R
+++ shinyR_HBCC_FigShare/app.R
@@ -10,7 +10,7 @@ library("iSEE")
 #To set an option
 options(timeout=600)
 # FigShare:
-dat <- ("https://figshare.com/ndownloader/files/39246662/sce_dlpfc_sgacc_final.RDS")
+dat <- ("https://figshare.com/ndownloader/files/39305303/sce_dlpfc_sgacc_final.RDS")
 download.file(dat, destfile = "sce_dlpfc_sgacc_final.RDS")
 sce_small <- readRDS("sce_dlpfc_sgacc_final.RDS")
 
