96365c070227c3a214bf11ffa26eb38b90f655aa
Ajeet <ajeetmdl23@gmail.com>
2023-02-17 16:30:45 -0500

    Update app.R

--- shinyR_HBCC_FigShare/app.R
+++ shinyR_HBCC_FigShare/app.R
@@ -86,50 +86,6 @@ initial[["ComplexHeatmapPlot1"]] <- new("ComplexHeatmapPlot", Assay = "logcounts
 # Settings for Feature assay plot 1
 ################################################################################
 
-initial[["FeatureAssayPlot1"]] <- new("FeatureAssayPlot", Assay = "logcounts", XAxis = "Column data", 
-                                      XAxisColumnData = "celltype", XAxisFeatureName = "SNAP25", 
-                                      XAxisFeatureSource = "---", XAxisFeatureDynamicSource = FALSE, 
-                                      YAxisFeatureName = "SNAP25", YAxisFeatureSource = "RowDataTable1", 
-                                      YAxisFeatureDynamicSource = TRUE, FacetRowByColData = "sample_ID", 
-                                      FacetColumnByColData = "sample_ID", ColorByColumnData = "region", 
-                                      ColorByFeatureNameAssay = "logcounts", ColorBySampleNameColor = "#FF0000", 
-                                      ShapeByColumnData = "sample_ID", SizeByColumnData = "frac.mito", 
-                                      FacetRowBy = "None", FacetColumnBy = "None", ColorBy = "Column data", 
-                                      ColorByDefaultColor = "#000000", ColorByFeatureName = "SNAP25", 
-                                      ColorByFeatureSource = "---", ColorByFeatureDynamicSource = FALSE, 
-                                      ColorBySampleName = "2543_sgACC_2_AAACCTGAGATAGGAG", ColorBySampleSource = "---", 
-                                      ColorBySampleDynamicSource = FALSE, ShapeBy = "None", SizeBy = "None", 
-                                      SelectionAlpha = 0.1, ZoomData = numeric(0), BrushData = list(), 
-                                      VisualBoxOpen = FALSE, VisualChoices = "Color", ContourAdd = FALSE, 
-                                      ContourColor = "#0000FF", PointSize = 1, PointAlpha = 1, 
-                                      Downsample = FALSE, DownsampleResolution = 200, CustomLabels = FALSE, 
-                                      CustomLabelsText = "2543_sgACC_2_AAACCTGAGATAGGAG", FontSize = 1, 
-                                      LegendPointSize = 1, LegendPosition = "Bottom", HoverInfo = TRUE, 
-                                      LabelCenters = FALSE, LabelCentersBy = "sample_ID", LabelCentersColor = "#000000", 
-                                      VersionInfo = list(iSEE = structure(list(c(2L, 6L, 0L)), class = c("package_version", 
-                                                                                                         "numeric_version"))), PanelId = c(FeatureAssayPlot = 1L), 
-                                      PanelHeight = 600L, PanelWidth = 12L, SelectionBoxOpen = FALSE, 
-                                      RowSelectionSource = "---", ColumnSelectionSource = "---", 
-                                      DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = FALSE, 
-                                      RowSelectionRestrict = FALSE, ColumnSelectionRestrict = TRUE, 
-                                      SelectionHistory = list())
-
-################################################################################
-# Settings for Row data table 1
-################################################################################
-
-initial[["RowDataTable1"]] <- new("RowDataTable", Selected = "SNAP25", Search = "", SearchColumns = "", 
-                                  HiddenColumns = character(0), VersionInfo = list(iSEE = structure(list(
-                                    c(2L, 6L, 0L)), class = c("package_version", "numeric_version"
-                                    ))), PanelId = c(RowDataTable = 1L), PanelHeight = 500L, 
-                                  PanelWidth = 6L, SelectionBoxOpen = FALSE, RowSelectionSource = "---", 
-                                  ColumnSelectionSource = "---", DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, 
-                                  ColumnSelectionDynamicSource = FALSE, RowSelectionRestrict = FALSE, 
-                                  ColumnSelectionRestrict = FALSE, SelectionHistory = list())
-
-
-
-
 ######################################
 
 sce_small <- registerAppOptions(sce_small, color.maxlevels = 47)
